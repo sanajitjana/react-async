0f06640d41b5e8c71b16f01b07259a36e522d36f
Sanajt Jana <sanajitjana1402@gmail.com>
2021-06-01 19:56:37 +0530

    add api with bootstarp table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Home";
 
 function App() {
--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,42 @@ import axios from "axios";
 
 const Home = () => {
   const [user, setUser] = useState([]);
-
   useEffect(() => {
-    console.log("Message");
+    loadUser();
   }, []);
 
+  const loadUser = async () => {
+    const result = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUser(result.data);
+  };
   return (
     <>
-      <h1>This is my Home Page</h1>
+      <div class="container mt-5">
+        <table class="table shadow table-hover">
+          <thead class="table-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <th scope="row">{user.name}</th>
+                <th scope="row">{user.email}</th>
+                <th scope="row">{user.phone}</th>
+                <th>Action</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
